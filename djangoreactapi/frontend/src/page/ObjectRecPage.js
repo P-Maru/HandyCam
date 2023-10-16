104890102b598deba917684294683b93ec7a184b
Seyeon <wjdtpdus828@naver.com>
2023-10-16 16:28:07 +0900

    정세연 / 물체인식기능

--- djangoreactapi/frontend/src/page/ObjectRecPage.js
+++ djangoreactapi/frontend/src/page/ObjectRecPage.js
@@ -1,45 +1,137 @@
-import "../style/LoginPage.css";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect, useRef, useCallback } from "react";
+import * as tf from "@tensorflow/tfjs";
+import "@tensorflow/tfjs-backend-webgl";
+import Loader from "../components/loader";
+import ButtonHandler from "../components/btn-handler";
+import { detectImage, detectVideo } from "../utils/detect";
+import { useNavigate } from "react-router-dom";
+import { renderBoxes } from "../utils/renderBox";
+import "../style/objectPage.css";
 
-function LoginPage() {
+function ObjectRecPage() {
+  const [loading, setLoading] = useState({ loading: true, progress: 0 });
+  const [model, setModel] = useState({
+    net: null,
+    inputShape: [1, 0, 0, 3],
+  });
+
+  const imageRef = useRef(null);
+  const cameraRef = useRef(null);
+  const videoRef = useRef(null);
+  const canvasRef = useRef(null);
+  const [labelValue, setLabelValue] = useState("");
+  const modelName = "phone";
+  const classThreshold = 0.2;
+
+
+  const handleButtonClick = () => {
+    alert(`인식한 물체는 ${labelValue} 입니다.`);
+    const url = videoRef.current.src;
+    videoRef.current.src = ""; // restore video source
+    videoRef.current.style.display = "none"; // hide video
+    // 또는 다른 사용자 정의 동작을 수행할 수 있습니다.
+
+    window.open('https://sldict.korean.go.kr/front/sign/signContentsView.do?origin_no=4399&top_category=CTE&category=&searchKeyword=%EC%A0%84%ED%99%94%EA%B8%B0&searchCondition=&search_gubun=&museum_type=00&current_pos_index=1');
+
+  };
+
+  useEffect(() => {
+    tf.ready().then(async () => {
+      const yolov5 = await tf.loadGraphModel(`../${modelName}_web_model/model.json`, {
+        onProgress: (fractions) => {
+          setLoading({ loading: true, progress: fractions });
+        },
+      });
+
+      const dummyInput = tf.ones(yolov5.inputs[0].shape);
+      const warmupResult = await yolov5.executeAsync(dummyInput);
+      tf.dispose(warmupResult);
+      tf.dispose(dummyInput);
+
+      setLoading({ loading: false, progress: 1 });
+      setModel({
+        net: yolov5,
+        inputShape: yolov5.inputs[0].shape,
+      });
+    });
+
+  }, []);
+
+  useEffect(() => {
+    console.log(labelValue);
+  }, [labelValue]);
+
+  console.log("Label Value in detectImageCallback:", labelValue);
   return (
-    <div
-      className="background"
-      style={{
-        backgroundImage: "url(./background.png)",
-        backgroundSize: "cover",
-        width: "100%",
-        height: "auto",
-        backgroundRepeat: "repeat-y",
-      }}
-    >
-      <form class="SignForm">
-        <h2 class="SignInHeader">HandyCam LogIn</h2>
-        <div class="SignContent">
-          <label>ID</label>
-          <input type="string" placeholder="아이디를 입력해주세요." />
+    <div className="ObjectRecPage">
+      {loading.loading && (
+        <Loader>Loading model... {(loading.progress * 100).toFixed(2)}%</Loader>
+      )}
+      <div className="header">
+        <h1>📷 YOLOv5 Live Detection App</h1>
+        <p>
+          YOLOv5 live detection application on the browser powered by{" "}
+          <code>tensorflow.js</code>
+        </p>
+        <p>
+          Serving: <code className="code">{modelName}</code>
+        </p>
+      </div>
+
+      <div className="content">
+        <img
+          src="#"
+          ref={imageRef}
+          onLoad={() =>
+            detectImage(
+              imageRef.current,
+              model,
+              classThreshold,
+              canvasRef.current,
+              setLabelValue
+            )
+          }
+        />
+        <video
+          autoPlay
+          muted
+          ref={cameraRef}
+          onPlay={() =>
+            detectVideo(
+              cameraRef.current,
+              model,
+              classThreshold,
+              canvasRef.current,
+              setLabelValue
+            )
+          }
+        />
+        <video
+          autoPlay
+          muted
+          ref={videoRef}
+          onPlay={() =>
+            detectVideo(
+              videoRef.current,
+              model,
+              classThreshold,
+              canvasRef.current,
+              setLabelValue
+            )
+          }
+        />
+        <canvas width={model.inputShape[1]} height={model.inputShape[2]} ref={canvasRef} />
+      </div>
+      {labelValue && (
+        <div className="label-value">
+          인식된 물체: {labelValue}
+          <br></br>
+          <button onClick={handleButtonClick}>인식한 물체 확인</button>
         </div>
-        <div class="SignContent">
-          <label>비밀번호</label>
-          <input type="password" placeholder="비밀번호를 입력해주세요." />
-        </div>
-        <Link to="/">
-          <div class="Btns">
-            <button type="submit" id="SignInBtn" class="Btn">
-              로그인
-            </button>
-          </div>
-        </Link>
-        <Link to="/SignUpPage">
-          <div class="Btns">
-            <button type="button" id="SingUpBtn" class="Btn">
-              회원가입
-            </button>
-          </div>
-        </Link>
-      </form>
+      )}
+      <ButtonHandler imageRef={imageRef} cameraRef={cameraRef} videoRef={videoRef} />
     </div>
   );
 }
 
-export default LoginPage;
+export default ObjectRecPage;
---
+++ djangoreactapi/frontend/src/utils/renderBox.js
@@ -0,0 +1,143 @@
+import labels from "./labels.json";
+import { useHistory } from 'react-router-dom';
+class Colors {
+  // ultralytics color palette https://ultralytics.com/
+  constructor() {
+    this.palette = [
+      "#FF3838",
+      "#FF9D97",
+      "#FF701F",
+      "#FFB21D",
+      "#CFD231",
+      "#48F90A",
+      "#92CC17",
+      "#3DDB86",
+      "#1A9334",
+      "#00D4BB",
+      "#2C99A8",
+      "#00C2FF",
+      "#344593",
+      "#6473FF",
+      "#0018EC",
+      "#8438FF",
+      "#520085",
+      "#CB38FF",
+      "#FF95C8",
+      "#FF37C7",
+    ];
+    this.n = this.palette.length;
+  }
+
+  get = (i) => this.palette[Math.floor(i) % this.n];
+
+  static hexToRgba = (hex, alpha) => {
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result
+      ? `rgba(${[parseInt(result[1], 16), parseInt(result[2], 16), parseInt(result[3], 16)].join(
+          ", "
+        )}, ${alpha})`
+      : null;
+  };
+}
+
+/**
+ * Render prediction boxes
+ * @param {HTMLCanvasElement} canvasRef canvas tag reference
+ * @param {number} classThreshold class threshold
+ * @param {Array} boxes_data boxes array
+ * @param {Array} scores_data scores array
+ * @param {Array} classes_data class array
+ * @param {Array[Number]} ratios boxes ratio [xRatio, yRatio]
+ * @param {Function} setLabelValue
+ */
+export const renderBoxes = (
+  canvasRef,
+  classThreshold,
+  boxes_data,
+  scores_data,
+  classes_data,
+  ratios,
+  setLabelValue
+) => {
+  const ctx = canvasRef.getContext("2d");
+  ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height); // clean canvas
+
+  const colors = new Colors();
+
+  // font configs
+  const font = `${Math.max(
+    Math.round(Math.max(ctx.canvas.width, ctx.canvas.height) / 40),
+    14
+  )}px Arial`;
+  ctx.font = font;
+  ctx.textBaseline = "top";
+
+  // Find the index with the highest score
+  let maxIndex = -1;
+  let maxScore = -1;
+
+  const validPredictions = [];
+
+  for (let i = 0; i < scores_data.length; ++i) {
+    // filter based on class threshold
+    if (scores_data[i] > classThreshold && scores_data[i] > maxScore) {
+      maxIndex = i;
+      maxScore = scores_data[i];
+    }
+  }
+
+  if (maxIndex !== -1) {
+    const klass = labels[classes_data[maxIndex]];
+    const color = colors.get(classes_data[maxIndex]);
+    const score = (maxScore * 100).toFixed(1);
+
+    let [x1, y1, x2, y2] = boxes_data.slice(maxIndex * 4, (maxIndex + 1) * 4);
+    x1 *= canvasRef.width * ratios[0];
+    x2 *= canvasRef.width * ratios[0];
+    y1 *= canvasRef.height * ratios[1];
+    y2 *= canvasRef.height * ratios[1];
+    const width = x2 - x1;
+    const height = y2 - y1;
+
+    // draw box.
+    ctx.fillStyle = Colors.hexToRgba(color, 0.2);
+    ctx.fillRect(x1, y1, width, height);
+    // draw border box.
+    ctx.strokeStyle = color;
+    ctx.lineWidth = Math.max(Math.min(ctx.canvas.width, ctx.canvas.height) / 200, 2.5);
+    ctx.strokeRect(x1, y1, width, height);
+
+    // Draw the label background.
+    ctx.fillStyle = color;
+    const textWidth = ctx.measureText(klass + " - " + score + "%").width;
+    const textHeight = parseInt(font, 10); // base 10
+    const yText = y1 - (textHeight + ctx.lineWidth);
+    ctx.fillRect(
+      x1 - 1,
+      yText < 0 ? 0 : yText, // handle overflow label box
+      textWidth + ctx.lineWidth,
+      textHeight + ctx.lineWidth
+    );
+
+    // Draw labels
+    ctx.fillStyle = "#ffffff";
+    ctx.fillText(klass + " - " + score + "%", x1 - 1, yText < 0 ? 0 : yText);
+    validPredictions.push({ label: `${klass}`, score: parseFloat(score) });
+  }
+
+  validPredictions.sort((a, b) => b.score - a.score);
+  console.log(validPredictions)
+  if (validPredictions.length > 0 && validPredictions[0].score > 60.0) {
+    console.log("hi");
+    console.log(typeof setLabelValue);
+    if (typeof setLabelValue === "function") {
+      setLabelValue(validPredictions[0].label); // 가장 높은 확률값만 가져오도록 수정
+      console.log("Label Value in renderBoxes:", validPredictions[0].label);
+      // const history = useHistory();
+      
+      // // 페이지 이동
+      // history.push('/www.google.com');
+    }
+
+  }
+};
